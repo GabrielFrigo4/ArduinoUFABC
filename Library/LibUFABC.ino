4f25ccae26ef421a49c9d1a4f2704579ce2446d5
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-07-14 12:36:55 -0300

    Update LibUFABC.ino

--- Library/LibUFABC.ino
+++ Library/LibUFABC.ino
@@ -10,15 +10,31 @@ void initSystem(){
   Serial.begin(9600);
 }
 
-void initWifi(){
-
-}
-
+// SENSOR
 void initSensor(Sensor &sensor){
   Serial.println("Lendo a umidade do solo..."); //IMPRIME O TEXTO NO MONITOR SERIAL
   delay(2000); //INTERVALO DE 2 SEGUNDOS
 }
 
+void updateSensor(Sensor &senso){
+  senso.valorLido = constrain(analogRead(senso.pinoSensor),senso.analogSoloMolhado,senso.analogSoloSeco); //MANTÉM valorLido DENTRO DO INTERVALO (ENTRE analogSoloMolhado E analogSoloSeco)
+  senso.valorLido = map(senso.valorLido,senso.analogSoloMolhado,senso.analogSoloSeco,senso.percSoloMolhado,senso.percSoloSeco); //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
+  Serial.print("Umidade do solo: "); //IMPRIME O TEXTO NO MONITOR SERIAL
+  Serial.print(senso.valorLido); //IMPRIME NO MONITOR SERIAL O PERCENTUAL DE UMIDADE DO SOLO
+  Serial.println("%"); //IMPRIME O CARACTERE NO MONITOR SERIAL
+  delay(1000);  //INTERVALO DE 1 SEGUNDO
+}
+
+// WIFI
+void initWifi(){
+
+}
+
+void updateWifi(){
+
+}
+
+// RADIO
 void initRadio(int mode){
   radio.begin();
   if(init == RADIO_READ){
@@ -34,19 +50,6 @@ void initRadio(int mode){
   else if(init == RADIO_WRITE){
     radio.stopListening(); 
   }  
-}
-
-void updateWifi(){
-
-}
-
-void updateSensor(Sensor &senso){
-  senso.valorLido = constrain(analogRead(senso.pinoSensor),senso.analogSoloMolhado,senso.analogSoloSeco); //MANTÉM valorLido DENTRO DO INTERVALO (ENTRE analogSoloMolhado E analogSoloSeco)
-  senso.valorLido = map(senso.valorLido,senso.analogSoloMolhado,senso.analogSoloSeco,senso.percSoloMolhado,senso.percSoloSeco); //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
-  Serial.print("Umidade do solo: "); //IMPRIME O TEXTO NO MONITOR SERIAL
-  Serial.print(senso.valorLido); //IMPRIME NO MONITOR SERIAL O PERCENTUAL DE UMIDADE DO SOLO
-  Serial.println("%"); //IMPRIME O CARACTERE NO MONITOR SERIAL
-  delay(1000);  //INTERVALO DE 1 SEGUNDO
 }
 
 void updateRadio(int mode){
