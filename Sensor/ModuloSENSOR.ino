ee6522b2bd0e0fdda1766b05694fd02857c37a3b
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-07-14 12:19:53 -0300

    Update ModuloSENSOR.ino

--- Sensor/ModuloSENSOR.ino
+++ Sensor/ModuloSENSOR.ino
@@ -8,58 +8,22 @@ https://github.com/mobizt/Firebase-ESP8266
 
 https://blogmasterwalkershop.com.br/arduino/como-usar-com-arduino-sensor-medidor-de-umidade-do-solo-higrometro
 https://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
+https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide
 ======================================================================================================================================= */
 
 /* INCLUDE */
-#include <Arduino.h>
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+#include "LibUFABC.h"
 
 /* DATA */
-RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00001";
-
-const int pinoSensor = A0; //PINO UTILIZADO PELO SENSOR
-int valorLido; //VARIÁVEL QUE ARMAZENA O PERCENTUAL DE UMIDADE DO SOLO
-
-int analogSoloSeco = 400; //VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
-int analogSoloMolhado = 150; //VALOR MEDIDO COM O SOLO MOLHADO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
-int percSoloSeco = 0; //MENOR PERCENTUAL DO SOLO SECO (0% - NÃO ALTERAR)
-int percSoloMolhado = 100; //MAIOR PERCENTUAL DO SOLO MOLHADO (100% - NÃO ALTERAR)
-
-/* FUNC */
-void initSensor();
-void initRadio();
+Sensor sensor;
 
 /* CODE */
 void setup(){
-  initSensor();
+  initSensor(&sensor);
   initRadio();
 }
 
 void loop(){
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
-
-  valorLido = constrain(analogRead(pinoSensor),analogSoloMolhado,analogSoloSeco); //MANTÉM valorLido DENTRO DO INTERVALO (ENTRE analogSoloMolhado E analogSoloSeco)
-  valorLido = map(valorLido,analogSoloMolhado,analogSoloSeco,percSoloMolhado,percSoloSeco); //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
-  Serial.print("Umidade do solo: "); //IMPRIME O TEXTO NO MONITOR SERIAL
-  Serial.print(valorLido); //IMPRIME NO MONITOR SERIAL O PERCENTUAL DE UMIDADE DO SOLO
-  Serial.println("%"); //IMPRIME O CARACTERE NO MONITOR SERIAL
-  delay(1000);  //INTERVALO DE 1 SEGUNDO
-}
-
-void initSensor(){
-  Serial.begin(9600); //INICIALIZA A SERIAL
-  Serial.println("Lendo a umidade do solo..."); //IMPRIME O TEXTO NO MONITOR SERIAL
-  delay(2000); //INTERVALO DE 2 SEGUNDOS
-}
-
-void initRadio(){
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  updateSensor(&sensor);
+  updateRadio();
 }
