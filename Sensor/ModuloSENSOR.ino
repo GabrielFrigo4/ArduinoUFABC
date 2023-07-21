df0aae1872ee5360ce83ad8bf7ef0f43a99be90b
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-07-21 12:54:45 -0300

    Update ModuloSENSOR.ino

--- Sensor/ModuloSENSOR.ino
+++ Sensor/ModuloSENSOR.ino
@@ -17,14 +17,33 @@ https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide
 /* DATA */
 Sensor sensor;
 
+const int pinoSensor = A0; //PINO UTILIZADO PELO SENSOR
+int valorLido; //VARIÁVEL QUE ARMAZENA O PERCENTUAL DE UMIDADE DO SOLO
+ 
+int analogSoloSeco = 623; //VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
+int analogSoloMolhado = 340; //VALOR MEDIDO COM O SOLO MOLHADO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
+int percSoloSeco = 0; //MENOR PERCENTUAL DO SOLO SECO (0% - NÃO ALTERAR)
+int percSoloMolhado = 100; //MAIOR PERCENTUAL DO SOLO MOLHADO (100% - NÃO ALTERAR)
+
 /* CODE */
 void setup(){
   initSystem();
   initSensor(&sensor);
   initRadio(RADIO_WRITE);
+
+  Serial.begin(9600); //INICIALIZA A SERIAL
+  Serial.println("Lendo a umidade do solo..."); //IMPRIME O TEXTO NO MONITOR SERIAL
+  delay(2000); //INTERVALO DE 2 SEGUNDOS
 }
 
 void loop(){
   updateSensor(&sensor);
   updateRadio(RADIO_WRITE);
+
+  valorLido = constrain(analogRead(pinoSensor),analogSoloMolhado,analogSoloSeco); //MANTÉM valorLido DENTRO DO INTERVALO (ENTRE analogSoloMolhado E analogSoloSeco)
+  valorLido = map(valorLido,analogSoloMolhado,analogSoloSeco,percSoloMolhado,percSoloSeco); //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
+  Serial.print("Umidade do solo: "); //IMPRIME O TEXTO NO MONITOR SERIAL
+  Serial.print(valorLido); //IMPRIME NO MONITOR SERIAL O PERCENTUAL DE UMIDADE DO SOLO
+  Serial.println("%"); //IMPRIME O CARACTERE NO MONITOR SERIAL
+  delay(100); 
 }
