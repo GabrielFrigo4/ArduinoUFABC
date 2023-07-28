01309c6f908f22497820ce9dbfbf5cd4f0288938
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-07-28 13:05:55 -0300

    Update FireBase.h

--- Internet/EspNET/FireBase.h
+++ Internet/EspNET/FireBase.h
@@ -1,5 +1,5 @@
 #define DATABASE_URL "https://flutter-temperatura-arduino-default-rtdb.firebaseio.com/"
 #define API_KEY "AIzaSyBl5TymsKLfMySzBm1DB5ue_oA2KcsdMco"
 
-void FireBase_Init();
+void FireBase_Init(const char* wifi_ssid, const char* wifi_password);
 void FireBase_SetInt(const char* path, int value);
