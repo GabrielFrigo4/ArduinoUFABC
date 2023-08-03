ed286325497eb5f16ad161e006ed0632254266cf
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-08-03 15:42:03 -0300

    Update LibUFABC.h

--- Library/LibUFABC/LibUFABC.h
+++ Library/LibUFABC/LibUFABC.h
@@ -3,18 +3,26 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+
+typedef unsigned char ubyte;
+
 #define RADIO_READ 0
 #define RADIO_WRITE 1
-typedef ubyte unsigned char;
+#define RADIO_BUFFER(x) (ubyte *)(x)
+#define RADIO_STRING(x) (const char *)(x)
+#define RADIO_TYPE(x) &x, sizeof(x)
 
 /* FUNC */
 void initSystem();
 void initRadio(int mode);
 void updateRadio(int mode);
 
-void getRadioBuffer(ubyte *buffer, int lenght, int offset_buffer, int offset_radio);
-void setRadioBuffer(ubyte *buffer, int lenght, int offset_buffer, int offset_radio);
+void getRadioBufferEx(ubyte *buffer, int lenght, int offset_buffer, int offset_radio);
+void setRadioBufferEx(ubyte *buffer, int lenght, int offset_buffer, int offset_radio);
+void getRadioBuffer(ubyte *buffer, int lenght);
+void setRadioBuffer(ubyte *buffer, int lenght);
 int getRaioBufferLenght();
 void cleanRadioBuffer();
 int getRadioBufferInt(int offset);
 void setRadioBufferInt(int value, int offset);
+void radioDelay(int mode);
