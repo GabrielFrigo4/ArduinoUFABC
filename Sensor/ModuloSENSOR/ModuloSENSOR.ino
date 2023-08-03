ed767778c526b3d2ddfd26a618d23727211ad491
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-08-03 15:58:06 -0300

    Update ModuloSENSOR.ino

--- Sensor/ModuloSENSOR/ModuloSENSOR.ino
+++ Sensor/ModuloSENSOR/ModuloSENSOR.ino
@@ -1,10 +1,4 @@
 /* =======================================================================================================================================
-https://github.com/mobizt/Firebase-ESP8266
-#if defined(PICO_RP2040)
-#include <WiFi.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#endif
 https://blogmasterwalkershop.com.br/arduino/como-usar-com-arduino-sensor-medidor-de-umidade-do-solo-higrometro
 https://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
 https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide
