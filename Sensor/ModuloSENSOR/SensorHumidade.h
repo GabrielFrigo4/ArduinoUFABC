670246d6e6eb3b8c2d8866b016464874c4147999
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-08-04 13:10:00 -0300

    Update SensorHumidade.h

--- Sensor/ModuloSENSOR/SensorHumidade.h
+++ Sensor/ModuloSENSOR/SensorHumidade.h
@@ -5,8 +5,8 @@
 struct Sensor
 {
   const int pinoSensor = A0;         // PINO UTILIZADO PELO SENSOR
-  const int analogSoloSeco = 623;    // VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
-  const int analogSoloMolhado = 340; // VALOR MEDIDO COM O SOLO MOLHADO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
+  const int analogSoloSeco = 909;    // VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
+  const int analogSoloMolhado = 360; // VALOR MEDIDO COM O SOLO MOLHADO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
   const int percSoloSeco = 0;        // MENOR PERCENTUAL DO SOLO SECO (0% - NÃO ALTERAR)
   const int percSoloMolhado = 100;   // MAIOR PERCENTUAL DO SOLO MOLHADO (100% - NÃO ALTERAR)
 
