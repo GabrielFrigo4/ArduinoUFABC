03d1481491ae1008ef0516fefd7cef69b9bce11b
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-08-04 13:09:04 -0300

    Update LibUFABC.cpp

--- Library/LibUFABC/LibUFABC.cpp
+++ Library/LibUFABC/LibUFABC.cpp
@@ -10,24 +10,26 @@ RF24 radio(1, 2); // CE, CSN
 RF24 radio(7, 8); // CE, CSN
 #endif
 
-#if BUFFER_SIZE
-ubyte radio_buffer[BUFFER_SIZE] = {0};
-int radio_buffer_lenght = BUFFER_SIZE;
-#else
-ubyte radio_buffer[32] = {0};
-const int radio_buffer_lenght = 32;
+/* DEFINE */
+#define BUFFER_TO_REALBUFFER(x) x-1
+#define REALBUFFER_TO_BUFFER(x) x+1
+#define BUFFERSIZE_TO_REALBUFFERSIZE(x) x+1
+#define REALBUFFERSIZE_TO_BUFFERSIZE(x) x-1
+#define BUFFER_INIT 0xFF
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 32
 #endif
+#define BUFFER_REAL_SIZE BUFFERSIZE_TO_REALBUFFERSIZE(BUFFER_SIZE)
+
+/* DATA */
+ubyte radio_buffer[BUFFER_REAL_SIZE] = {0};
 const uint64_t address = ADDRESS;
+bool is_data_updated = false;
 
 /* CODE */
 void initSystem()
 {
   Serial.begin(9600);
-#if ESP8266
-  Serial.println("ESP8266");
-#else
-  Serial.println("ARDUINO");
-#endif
 }
 
 // RADIO
@@ -56,79 +58,105 @@ void initRadio(int mode)
 }
 
 void updateRadio(int mode)
-{
+{  
   if (mode == RADIO_WRITE)
   {
-    radio.write(radio_buffer, radio_buffer_lenght);
+    radio.write(radio_buffer, BUFFER_SIZE);
   }
 
   if (mode == RADIO_READ)
   {
-    radio.read(radio_buffer, radio_buffer_lenght);
+    ubyte buf[BUFFER_REAL_SIZE] = {0};
+    radio.read(buf, BUFFER_REAL_SIZE);
+    if(buf[0] == BUFFER_INIT){
+      radioCopyMemory(buf, radio_buffer, BUFFER_REAL_SIZE, BUFFER_REAL_SIZE, 0, 0, BUFFER_REAL_SIZE);
+      is_data_updated = true;
+    }
+    else {
+      is_data_updated = false;
+    }
   }
 }
 
 // RADIO BUFFER
-void getRadioBufferEx(ubyte *buffer, int lenght, int offset_buffer, int offset_radio)
+void getRadioBufferEx(ubyte *buffer, int lenght_buffer, int offset_buffer, int offset_radio, int lenght)
 {
-  if (lenght > radio_buffer_lenght - offset_radio)
-  {
-    lenght = radio_buffer_lenght - offset_radio;
-  }
-  for (int i = 0; i < lenght; i++)
-  {
-    buffer[i + offset_buffer] = radio_buffer[i + offset_radio];
-  }
+  radioCopyMemory(REALBUFFER_TO_BUFFER(radio_buffer), buffer, BUFFER_SIZE, lenght_buffer, offset_radio, offset_buffer, lenght);
 }
 
-void setRadioBufferEx(ubyte *buffer, int lenght, int offset_buffer, int offset_radio)
+void setRadioBufferEx(ubyte *buffer, int lenght_buffer, int offset_buffer, int offset_radio, int lenght)
 {
-  if (lenght > radio_buffer_lenght - offset_radio)
-  {
-    lenght = radio_buffer_lenght - offset_radio;
-  }
-  for (int i = 0; i < lenght; i++)
-  {
-    radio_buffer[i + offset_radio] = buffer[i + offset_buffer];
-  }
+  radio_buffer[0] = BUFFER_INIT;
+  radioCopyMemory(buffer, REALBUFFER_TO_BUFFER(radio_buffer), lenght_buffer, BUFFER_SIZE, offset_buffer, offset_radio, lenght);
 }
 
 void getRadioBuffer(ubyte *buffer, int lenght)
 {
-  getRadioBufferEx(buffer, lenght, 0, 0);
+  getRadioBufferEx(buffer, lenght, 0, 0, lenght);
 }
 
 void setRadioBuffer(ubyte *buffer, int lenght)
 {
-  setRadioBufferEx(buffer, lenght, 0, 0);
+  setRadioBufferEx(buffer, lenght, 0, 0, lenght);
 }
+
+int getRadioBufferIntEx(int offset)
+{
+  int value;
+  getRadioBufferEx(RADIO_BUFFER(&value), sizeof(int), 0, offset, sizeof(int));
+  return value;
+}
+
+void setRadioBufferIntEx(int value, int offset)
+{
+  setRadioBufferEx(RADIO_BUFFER(&value), sizeof(int), 0, offset, sizeof(int));
+}
+
+int getRadioBufferInt()
+{
+  return getRadioBufferIntEx(0);
+}
+
+void setRadioBufferInt(int value)
+{
+  setRadioBufferIntEx(value, 0);
+}
+
 
 int getRaioBufferLenght()
 {
-  return radio_buffer_lenght;
+  return BUFFER_SIZE;
+}
+
+bool getIsDataUpdated(){
+  return is_data_updated;
 }
 
 void cleanRadioBuffer()
 {
-  for (int i = 0; i < radio_buffer_lenght; i++)
+  for (int i = 0; i < BUFFER_SIZE; i++)
   {
     radio_buffer[i] = 0;
   }
 }
 
-int getRadioBufferInt(int offset)
-{
-  int value;
-  getRadioBufferEx(RADIO_BUFFER(&value), sizeof(int), 0, offset);
-  return value;
-}
-
-void setRadioBufferInt(int value, int offset)
-{
-  setRadioBufferEx(RADIO_BUFFER(&value), sizeof(int), 0, offset);
-}
-
 // RADIO UTILS
+void radioCopyMemory(ubyte *buffer_source, ubyte *buffer_copy, int lenght_source, int lenght_copy, int offset_source, int offset_copy, int lenght)
+{
+  if (lenght > lenght_source - offset_source)
+  {
+    lenght = lenght_source - offset_source;
+  }
+  if (lenght > lenght_copy - offset_copy)
+  {
+    lenght = lenght_copy - offset_copy;
+  }
+  for (int i = 0; i < lenght; i++)
+  {
+    buffer_copy[i + offset_copy] = buffer_source[i + offset_source];
+  }
+}
+
 void radioDelay(int mode)
 {
   if (mode == RADIO_READ)
