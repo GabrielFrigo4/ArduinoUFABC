ce77d441a0e427c4802f8c7e23da0fdadf82c7be
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-08-04 13:08:47 -0300

    Update LibUFABC.h

--- Library/LibUFABC/LibUFABC.h
+++ Library/LibUFABC/LibUFABC.h
@@ -4,8 +4,10 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+/* TYPE */
 typedef unsigned char ubyte;
 
+/* DEFINE */
 #define RADIO_READ 0
 #define RADIO_WRITE 1
 #define RADIO_BUFFER(x) (ubyte *)(x)
@@ -13,16 +15,22 @@ typedef unsigned char ubyte;
 #define RADIO_TYPE(x) &x, sizeof(x)
 
 /* FUNC */
+// RADIO
 void initSystem();
 void initRadio(int mode);
 void updateRadio(int mode);
-
-void getRadioBufferEx(ubyte *buffer, int lenght, int offset_buffer, int offset_radio);
-void setRadioBufferEx(ubyte *buffer, int lenght, int offset_buffer, int offset_radio);
+// RADIO BUFFER
+void getRadioBufferEx(ubyte *buffer, int lenght_buffer, int offset_buffer, int offset_radio, int lenght);
+void setRadioBufferEx(ubyte *buffer, int lenght_buffer, int offset_buffer, int offset_radio, int lenght);
 void getRadioBuffer(ubyte *buffer, int lenght);
 void setRadioBuffer(ubyte *buffer, int lenght);
+int getRadioBufferIntEx(int offset);
+void setRadioBufferIntEx(int value, int offset);
+int getRadioBufferInt();
+void setRadioBufferInt(int value);
 int getRaioBufferLenght();
+bool getIsDataUpdated();
 void cleanRadioBuffer();
-int getRadioBufferInt(int offset);
-void setRadioBufferInt(int value, int offset);
+// RADIO UTILS
+void radioCopyMemory(ubyte *buffer_source, ubyte *buffer_copy, int lenght_source, int lenght_copy, int offset_source, int offset_copy, int lenght);
 void radioDelay(int mode);
