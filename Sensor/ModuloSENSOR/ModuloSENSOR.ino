37f2ac3295f4ea5df0385707122811f6c0264bb1
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-08-04 13:09:39 -0300

    Update ModuloSENSOR.ino

--- Sensor/ModuloSENSOR/ModuloSENSOR.ino
+++ Sensor/ModuloSENSOR/ModuloSENSOR.ino
@@ -25,11 +25,10 @@ void loop()
   updateSensor(sensor);
   updateRadio(RADIO_WRITE);
 
-  ubyte buf[16] = "Hello World!!";
-  setRadioBuffer(buf, sizeof(buf));
-
   Serial.print("Umidade do solo: "); // IMPRIME O TEXTO NO MONITOR SERIAL
   Serial.print(sensor.valorLido);    // IMPRIME NO MONITOR SERIAL O PERCENTUAL DE UMIDADE DO SOLO
   Serial.println("%");               // IMPRIME O CARACTERE NO MONITOR SERIAL
+  setRadioBufferInt(sensor.valorLido);
+
   radioDelay(RADIO_WRITE);
 }
