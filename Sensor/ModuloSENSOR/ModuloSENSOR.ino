9a035491dbf8245201b74fe50de756798b0568aa
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-08-04 13:59:59 -0300

    Update ModuloSENSOR.ino

--- Sensor/ModuloSENSOR/ModuloSENSOR.ino
+++ Sensor/ModuloSENSOR/ModuloSENSOR.ino
@@ -1,9 +1,11 @@
 /* =======================================================================================================================================
 https://blogmasterwalkershop.com.br/arduino/como-usar-com-arduino-sensor-medidor-de-umidade-do-solo-higrometro
 https://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
-https://eletronicaparaartistas.com.br/arduino-2-configuracao-do-arduino-nano/
 https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide
 https://maniacbug.github.io/RF24/classRF24.html
+
+https://br-arduino.org/2014/12/arduino-e-bateria-providencias-simples-para-reduzir-o-consumo.html
+https://eletronicaparaartistas.com.br/arduino-2-configuracao-do-arduino-nano/
 ======================================================================================================================================= */
 
 /* INCLUDE */
